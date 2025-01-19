1523f88988a0d1ace871bf19aa535ddfe147c108
Rafael Magaña <raismaav.2510@gmail.com>
2025-01-18 23:33:39 -0600

    Initial Commit

---
+++ Cube App/Cube_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cube_AppApp.swift
+//  Cube App
+//
+//  Created by Rafael Magana  on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Cube_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
